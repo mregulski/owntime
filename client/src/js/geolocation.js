2f5e5158e9a6a62f01ec6443efa9632c47ec8452
Marcin Regulski <maarcin.regulski@gmail.com>
2016-12-09 18:39:09 +0100

    rewrite geolocation to use Promises, cleanup

--- client/src/js/geolocation.js
+++ client/src/js/geolocation.js
@@ -10,38 +10,16 @@
 
     app.geolocation = geolocation;
 
-
-    function getCurrentPosition(success) {
-        navigator.geolocation.getCurrentPosition(success, handleError, {
-            enableHighAccuracy: true,
-            timeout: 5000,
-            maximumAge: 36000
-        })
+    /**
+     * Return a Promise with user's current location
+     */
+    function getCurrentPosition() {
+        return new Promise((resolve, reject) => {
+                navigator.geolocation.getCurrentPosition(resolve, reject, {
+                enableHighAccuracy: true,
+                timeout: 5000,
+                maximumAge: 36000
+            });
+        });
     }
-
-    function handleError(error) {
-        log.e(error);
-    }
-
 })(app);
-
-
-var map = new Vue({
-    el: '#location',
-    data: {
-        coords: {}
-    }
-});
-
-// test
-$(document).ready(() => {
-    app.geolocation.getCurrentPosition((result) => {
-        window.result = result;
-        let latitude = result.coords.latitude;
-        let longitude = result.coords.longitude;
-        let map = "https://maps.googleapis.com/maps/api/staticmap?markers=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false"
-        var img = $("<img>", {id: "map"})
-            .attr("src", map);
-        $("#location").append(img);
-    });
-});
