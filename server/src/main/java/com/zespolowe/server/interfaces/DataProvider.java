555eac3d7d9d786ee8862ad3bd274f7fd2e9ea5a
cirben <221450@student.pwr.edu.pl>
2016-12-14 15:37:31 +0100

    Corrected comments for dataprovider interface

--- server/src/main/java/com/zespolowe/server/interfaces/DataProvider.java
+++ server/src/main/java/com/zespolowe/server/interfaces/DataProvider.java
@@ -21,15 +21,15 @@ public interface DataProvider {
 	* @return unordered ArrayList of all elementary connections with unique id for future updating of specific Connection. Example,
     * all planned departures and arrivals for all pairs of connected points. That includes connections for all kinds of transport 
     * except for on foot Connections which are computed and added during the model creation.
-    * elementary Connection should be added for ex. For Next 24 hours period. Then during each update new connections should be
+    * elementary Connections should be added for ex. For Next 24 hours period. Then during each update new connections should be
     * added so length of the period is maintained.
     */
     ArrayList<Connection> getConnections();
 
     /** 
     * @return unordered ArrayList of new and updated Connections which are updated by their unique id. During update process 
-    * Times of departure and arrival are update in coresponding connections. Ex. Real data prediction alg. predicts new arrival 
-    * time and new Connection with specific id and updated times will be passed 
+    * Times of departure and arrival are updated in coresponding connections. Ex. Real data prediction alg. predicts new arrival 
+    * time and new Connection with specific id(of Connection to be updated) and updated times will be passed 
     * through this function.
     */
     ArrayList<Connection> getUpdatedConnections();
