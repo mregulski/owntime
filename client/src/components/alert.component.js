99219cb10fb18720fd6fc1da79535e9fd4bcb570
Marcin Regulski <maarcin.regulski@gmail.com>
2016-12-19 20:36:13 +0100

    remove duplicate property in `alert.component`

--- client/src/components/alert.component.js
+++ client/src/components/alert.component.js
@@ -14,13 +14,12 @@
             'activeRoute'
         ],
         data: function() { return {
-                route: this.activeRoute,
                 minutes: 0,
                 alertSet: false
             }},
         methods: {
             addAlert: function() {
-                app.notification.setAlarm(moment().add(5, 'seconds'), this.route);
+                app.notification.setAlarm(moment().add(5, 'seconds'), this.activeRoute);
                 this.alertSet = true
             }
         }
