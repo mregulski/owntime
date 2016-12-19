530978b1e2cdb2dcabaf509cfce934fd5beccbe7
R3R3 <fiat126pelx@wp.pl>
2016-12-20 00:44:58 +0100

    initMap

--- client/src/js/initMap.js
+++ client/src/js/initMap.js
@@ -13,8 +13,10 @@ to triggerowało funkcję w html'u
         var geoloc = app.geolocation.getCurrentPosition();
         var mapOptions = {
             zoom: 17,
-            center: geoloc /*{lat: 51.10930159999999, lng:  17.0517307}*/
+            center: /*geoloc*/ {lat: 51.10930159999999, lng:  17.0517307}
         };
         map = new google.maps.Map(document.getElementById('map'), mapOptions);
     }
 })(app);
+
+function mapsCallback () {console.log("mapsCallback");app.initMap();};
