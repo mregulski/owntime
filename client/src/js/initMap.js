a9ead5112937aad5051b20042925767a612027e0
R3R3 <fiat126pelx@wp.pl>
2016-12-20 01:32:49 +0100

    Working initMap

--- client/src/js/initMap.js
+++ client/src/js/initMap.js
@@ -1,22 +1,28 @@
-/*
-to triggerowało funkcję w html'u
-
-<script async defer
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBSrkPCHS0QJt0aVyG_2rNDEmLJ2dZ_KnU&callback=initMap">
-    </script>
-*/
 (function(app) {
     let log = app.getLog("initMap");
     let initMap = {}
 
-     app.initMap = function initialMap() {
+     app.initMap = function () {
         var geoloc = app.geolocation.getCurrentPosition();
-        var mapOptions = {
+        coords = {};
+        geoloc.then(function(result){
+            coords.lat = result.coords.latitude;
+            coords.lng = result.coords.longitude;
+            var mapOptions = {
+            zoom: 15,
+            center: coords
+            };
+            map = new google.maps.Map(document.getElementById('map'), mapOptions);
+        }, function(err){
+            coords = {lat: 51.110909, lng: 17.056910};
+            var mapOptions = {
             zoom: 17,
-            center: /*geoloc*/ {lat: 51.10930159999999, lng:  17.0517307}
-        };
-        map = new google.maps.Map(document.getElementById('map'), mapOptions);
+            center: coords
+            };
+            map = new google.maps.Map(document.getElementById('map'), mapOptions);
+        });
+
     }
 })(app);
 
-function mapsCallback () {console.log("mapsCallback");app.initMap();};
+function mapsCallback () {app.initMap();};
