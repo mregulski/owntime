247ed4134aa84a016ca0845586546291b9d89988
Zeimer <zzaimer@gmail.com>
2017-01-14 23:36:41 +0100

    Wczytywanie danych o opóźnieniach — póki co dupy nie urywa.

--- server/src/main/java/com/zespolowe/server/dataFormats/PointService.java
+++ server/src/main/java/com/zespolowe/server/dataFormats/PointService.java
@@ -8,4 +8,10 @@ public class PointService {
     public Point getById(int id){
         return new Point(id, "kot"+id, new Coords(id, id));
     }
+    
+    // Same stuff as above, but no need to create instances of this sad class. Yay!
+    public static Point staticGetById(int id)
+    {
+    	return new Point(id, "kot"+id, new Coords(id, id));
+    }
 }
