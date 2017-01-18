e5a3c790d8d683b95741efecb02b25940bf2ea22
Maciej Fitowski <maciej@vitrius.co>
2017-01-18 10:22:20 +0100

    Add method to get trips ids

---
+++ data/src/additionalInfo/Trips.java
@@ -0,0 +1,33 @@
+package additionalInfo;
+
+import SQL.BaseDAO;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class Trips {
+
+    /**
+     * Gets all lines
+     * @return
+     * @throws SQLException
+     * @throws IOException
+     */
+    public ArrayList getAllTripsIds() throws SQLException, IOException {
+        Connection conn = new BaseDAO().getConnection();
+        Statement stat = conn.createStatement();
+        ResultSet rs = stat.executeQuery("SELECT trip_id FROM trips GROUP BY trip_id");
+
+        ArrayList ids = new ArrayList<>();
+        while(rs.next()) {
+            ids.add(rs.getString(1));
+        }
+        conn.close();
+
+        return ids;
+    }
+}
