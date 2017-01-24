be9d239f4934daf0767f3d0126abe48a9dd8b223
lizzard888 <lizzard@autograf.pl>
2017-01-24 01:46:51 +0100

    Zmiany kosmetyczne w klasie Donald Trump.

--- server/src/main/java/com/zespolowe/server/dataFormats/DonaldTrumpRequestProvider.java
+++ server/src/main/java/com/zespolowe/server/dataFormats/DonaldTrumpRequestProvider.java
@@ -1,12 +1,7 @@
 package com.zespolowe.server.dataFormats;
 
-import com.zespolowe.server.PathFindingClasses.VConnection;
 import com.zespolowe.server.interfaces.RequestProvider;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.List;
-import static java.time.LocalDateTime.now;
-import java.util.Collections;
 
 /**
  * Created by lizzard on 19.01.17. Mamy dwie listy działające na zasadzie
@@ -30,24 +25,19 @@ public class DonaldTrumpRequestProvider implements RequestProvider {
 		isReady = false;
 	}
 
+    @Override
 	public synchronized void addPath(Path path) {
 		// TYM WRZUCAĆ OBLICZONE PATHY!!!
 		isReady = true;
 		pathList.add(path);
 	}
 
-	public synchronized Result getResult() { //TODO Przetworzenie Path ze stosu na obiekt Result
-		Result result;
+	public synchronized Result getResult() {
 		if (pathList.size() == 1) {
 			isReady = false;
 		}
 		Path path = pathList.remove(pathList.size() - 1);
-		// FEEEEEEEEEEEEEJK!!!
 		return Path.toResult(path);
-		/*
-        ArrayList<Node> route = new ArrayList<>();
-        result = new Result(new Point(0), new Point(0), route);
-        return result;*/
 	}
 
 	public synchronized boolean isProcessingFinished() {
@@ -69,6 +59,7 @@ public class DonaldTrumpRequestProvider implements RequestProvider {
 
 	@Override
 	public synchronized boolean shouldStop() {
-		return (!isProcessing);
+		//return (!isProcessing);
+        return false;
 	}
 }
