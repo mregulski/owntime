6f3e7c206ae3b7306f90ad1db5ad6112133b97fd
Marcin Regulski <maarcin.regulski@gmail.com>
2017-01-26 00:05:48 +0100

    fix search, routeInfo display

--- client/src/components/routeInfo.component.js
+++ client/src/components/routeInfo.component.js
@@ -3,30 +3,49 @@
     Vue.component('yak-route', {
         template: `
         <div class="searchbox__message">
+            <div v-if="routeSelected">
                 <p>Linia <span class="chosen-connection">{{connection}}</span> w kierunku <span
-                        class="chosen-destination">{{destination}}</span> o <span class="chosen-starting-time">{{startTime.planned}}</span></p>
+                        class="chosen-destination">{{destination}}</span> o <span class="chosen-starting-time">{{startTime}}</span></p>
                 <p>Do wyjazdu pozostało Ci</p>
                 <p><span class="chosen-starting-time-left">{{timeLeft}}</span></p>
                 <p>Na miejsce przyjedziesz najpóźniej o
-                    <span class="chosen-arrival-time">{{arrivalTime.planned}} {{arrivalTime.estimated}}</span>
+                    <span class="chosen-arrival-time">{{arrivalTime}} {{arrivalTime}}</span>
                 </p>
             </div>
+        </div>
         `,
         props: [ 'route' ],
         data: function() { return {}},
         computed: {
+            /**
+             * Check if there is a route by checking times.
+             */
+            routeSelected: function() {
+                this.log("routeSelected:",this.startTime && this.arrivalTime)
+                return this.startTime && this.arrivalTime;
+            },
             connection: function() {
-                return _.get(this, 'route.route[0].transport.line', "");
+                return _.get(this, 'route.route[0].transport.details.line', "");
 
             },
             destination: function() {
-                return _.get(this, 'route.end', "");
+                return _.get(this, 'route.end.displayName', "");
             },
             startTime: function() {
-                return _.get(this, 'route.departure', {planned: "plan", predicted: "estimate"});
+                // make sure there IS a ctime to display
+                let time = _.get(this, 'route.departure', "");
+                if (time == "") return "";
+
+                this.log("startTime:", time);
+                return moment(time.planned).format("HH:mm");
+                // return {};
             },
             arrivalTime: function() {
-                return _.get(this, 'route.arrival', {planned: "plan", predicted: "estimate"});
+                let time = _.get(this, 'route.arrival', "");
+                if (time == "") return "";
+                this.log("arrivalTime:", time);
+                return moment(time.predicted).format("HH:mm");
+
             },
             timeLeft: function() {
                 // startTime = _.get(this, 'route.departure', null);
@@ -36,7 +55,7 @@
 
         },
         methods: {
-            log: app.getLog('yak-route')
+            log: app.getLog('route')
         }
     });
 })(app);
