8f62cca2dbebc6b99da8ce55e520559521c5be48
cirben <221450@student.pwr.edu.pl>
2017-01-25 21:38:26 +0100

    using new test request provider

--- server/src/main/java/com/zespolowe/server/main.java
+++ server/src/main/java/com/zespolowe/server/main.java
@@ -1,5 +1,6 @@
 package com.zespolowe.server;
 
+import com.zespolowe.server.PathFindingClasses.ConsoleRequestProvider;
 import com.zespolowe.server.PathFindingClasses.PathFinder;
 import com.zespolowe.server.dataFormats.JsonConnection;
 import com.zespolowe.server.dataFormats.MariaDBDataProvider;
@@ -12,20 +13,22 @@ public class main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args){
+    public static void main(String[] args) throws InterruptedException{
 
         //ReallyBadExampleOfFakeDataAndRequestProviderInOneClass noob = new ReallyBadExampleOfFakeDataAndRequestProviderInOneClass();
-        /*MariaDBDataProvider dataProvider = new MariaDBDataProvider();
-        DonaldTrumpRequestProvider requestProvider = new DonaldTrumpRequestProvider();
+        ConsoleRequestProvider crp = new ConsoleRequestProvider();
+		MariaDBDataProvider dataProvider = new MariaDBDataProvider();
+        //DonaldTrumpRequestProvider requestProvider = new DonaldTrumpRequestProvider();
         PathFinder pf = new PathFinder();
         pf.setDataProvider(dataProvider);
-        pf.setRequestProvider(requestProvider);
+        //pf.setRequestProvider(requestProvider);
+        pf.setRequestProvider(crp);
         pf.init();
 
         pf.run();
-*/
-        JsonConnection jsonConnection = new JsonConnection();
-        jsonConnection.configureRESTPointHandlers();
+		//pf.wait();
+        //JsonConnection jsonConnection = new JsonConnection();
+        //jsonConnection.configureRESTPointHandlers();
 
 //        Spark.get("/hello", (req, res) -> "Hello World");
     }
